b5d1ec2e1f4a19746f98854578f714c31884a264
Omar Benjamin Ramirez Castillo <omarbenjaminrc@gmail.com>
2025-04-21 12:36:42 -0400

    se creó el modelo articulos y se ejecutaron migraciones

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Articulo(models.Model):
+    titulo = models.CharField(max_length= 90)
+    contenido = models.TextField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=100,unique=True,db_index=True,blank=True)
+
+    def __str__(self):
+        return self.titulo
+    
+    def save(self, *args, **kwargs): 
+        '''
+        Sobrescribe el método save original.
+        Crea un slug automaticamente si no existe uno al guardar el articulo.
+        '''
+        if not self.slug: 
+            self.slug = slugify(self.titulo)
+        super().save(*args, **kwargs) 
+
+    def get_absolute_url(self):
+        '''
+        Devuelve la URL absoluta para una instancia de Articulo.
+        '''
+        from django.urls import reverse
+        
+        return reverse('detalle_articulo', kwargs={'slug': self.slug})
